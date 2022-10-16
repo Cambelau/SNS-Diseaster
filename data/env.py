7785b1a13922f90714f544f53f4ba9260c73ea72
Cambelau <sajotmatthieu@gmail.com>
2022-10-16 17:58:22 +0200

    init repo

---
+++ data/env.py
@@ -0,0 +1,14 @@
+import os
+
+os.environ['consumer_key'] = "xoXS9SUwolzz3zUxICZAdaL7v"
+os.environ['consumer_secret'] = 'g1zvMJHIXxU3XUSlCmxy9mUjM4RjbwefILNCWvegMXgahm1vnS'
+os.environ['access_token'] = '3242472841-0fHGNzLjIWdNyg0FFRtPWhxvNVSe5MJ2XYmQZud'
+os.environ['access_token_secret'] = 'VsWUWevN1VLzHg0rBpHee4A6nvbIObEv1TsLHREmAYwGL'
+os.environ['BEARER_TOKEN'] = "AAAAAAAAAAAAAAAAAAAAADbXfAEAAAAArUlnd0nX%2FLi03Cw4%2FfuslWM9vAE%3D5D1lJ597d0HJzjw48j7HtjsQKNdDntHzdUNdPEdmE8hDMwylxV"
+consumer_key = os.environ.get('consumer_key')
+consumer_secret = os.environ.get('consumer_secret')
+access_token = os.environ.get('access_token')
+access_token_secret = os.environ.get('access_token_secret')
+bearer_token = os.getenv("BEARER_TOKEN")
+
+bearer_token_academic="AAAAAAAAAAAAAAAAAAAAAJm%2BcgEAAAAAXYpKw0GK0tQtQH0coQVXw1SK%2Fwc%3DhMlBR1QNbP6mLXyEOqTkkmlsVkZ9G9w1zFlzY8niSbnF2iAoAu"
